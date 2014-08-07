d3b4585118df49fcddeff17e5e2aaec9f1646091
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-08-06 23:53:33 -0400

    Added sentiment.py
    Added basic naive classifier, trained on movie reviews and probably not
    very good at this. But that's ok. It's pretty easy to play with the
    python nltk library.
    
    Something we can do wth this is generate JSON to use in visulations of
    Locations and maps and things. That will be fun.

---
+++ sentiment.py
@@ -0,0 +1,66 @@
+#pip install lxml
+from lxml import etree
+
+#pip install nltk
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+
+#nltk.download()
+from nltk.corpus import movie_reviews
+
+datafiles = [
+	"14-28-RAW-Solr-1.xml",
+	"14-28-RAW-Solr-2.xml",
+	"14-28-RAW-Solr-3.xml"
+	"14-28-RAW-Solr-4.xml"
+	"14-28-RAW-Solr-5.xml"
+]
+
+#Set this to the folder path where you unzipped the xml datasets
+datadir = "unzipped"
+
+
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
+def trainNaiveClassifier():
+	negids = movie_reviews.fileids('neg')
+	posids = movie_reviews.fileids('pos')
+	 
+	negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+	posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+	 
+	negcutoff = len(negfeats)*3/4
+	poscutoff = len(posfeats)*3/4
+	 
+	trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+	testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+	print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+	 
+	classifier = NaiveBayesClassifier.train(trainfeats)
+	print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+	classifier.show_most_informative_features()
+	return classifier
+
+def main():
+	classifier = trainNaiveClassifier()
+	doms = []
+	for datafile in datafiles:
+		print "Reading data file: {}".format(datafile)
+		doms.append( etree.parse("{}/{}".format(datadir,datafile) ) )
+		break #break for now until we figure out how to deal with xml file 2
+	
+	for dom in doms:
+		comments = dom.getroot().getchildren()[1]
+		for comment in comments:
+			print classifier.classify(  word_feats(comment.xpath("arr[@name = 'text']/str")[0]) )
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+ 
+
+ 
